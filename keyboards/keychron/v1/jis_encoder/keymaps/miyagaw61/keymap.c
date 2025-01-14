e016523f720905ba605ce7d42d033d74e6c12d24
miyagawa <Taisei.Miyagawa@sony.com>
2025-01-14 19:43:37 +0900

    add keybinds for vscode

--- keyboards/keychron/v1/jis_encoder/keymaps/miyagaw61/keymap.c
+++ keyboards/keychron/v1/jis_encoder/keymaps/miyagaw61/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          _______,  _______,  _______,  _______,                      _______,                      _______,  _______,  _______,    _______,  _______,  _______,  _______),
 
     [WIN_BASE] = LAYOUT_jis_86(
-         KC_ESC,              KC_F1,    KC_F2,                   KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,       KC_F8,     KC_F9,         KC_F10,      KC_F11,         KC_F12,    KC_DEL,    LSFT(LCTL(KC_M)),
+         KC_ESC,              KC_F1,    KC_F2,                   KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,       KC_F8,     KC_F9,         KC_F10,      KC_F11,         KC_F12,    KC_DEL,    LSFT(LCTL(KC_K)),
          KC_GRV,    KC_1,     KC_2,     KC_3,                    KC_4,     KC_5,     KC_6,     KC_7,     KC_8,        KC_9,      KC_0,          KC_MINS,     LSFT(KC_MINS),  KC_INT3,   KC_BSPC,   KC_PGUP,
          KC_TAB,    KC_Q,     KC_W,     KC_E,                    KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,        KC_O,      KC_P,          KC_RBRC,     KC_NUHS,                              KC_PGDN,
          KC_LCTL,   KC_A,     KC_S,     KC_D,                    KC_F,     KC_G,     KC_H,     KC_J,     KC_K,        KC_L,      KC_SCLN,       LSFT(KC_7),  KC_NUHS,        KC_ENT,               KC_HOME,
@@ -559,6 +559,7 @@ bool process_mod1_keys(uint16_t keycode, keyrecord_t *record) {
         PROC_MOD1(LSFT(KC_7), LSFT(KC_LBRC));  // MOD1+'  ->  `
         PROC_MOD1(KC_SLSH,    LSFT(KC_EQL));   // MOD1+/  ->  ~
         PROC_MOD1(KC_D,       LCTL(KC_F1));    // MOD1+D  ->  CTL+F1
+        PROC_MOD1(KC_X,       LSFT(LCTL(KC_K)));    // MOD1+X  ->  SFT+CTL+K
         PROC_MOD1_2(KC_I,    KC_I, KC_INT5);   // MOD1+I  ->  I, MOD1-ONESHOT
         PROC_MOD1_2(KC_O,    KC_O, KC_INT5);   // MOD1+O  ->  O, MOD1-ONESHOT
         PROC_MOD1_2(KC_A,    KC_A, KC_INT5);   // MOD1+A  ->  A, MOD1-ONESHOT
